6a27e456efeb7c2715c2998b769971f0c5b07fc0
M Montserrat Gomez <gome0145@algonquinlive.com>
2023-08-14 14:09:51 -0400

    Initial Commit

---
+++ WeatherEjercicio/WeatherEjercicioApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherEjercicioApp.swift
+//  WeatherEjercicio
+//
+//  Created by Montserrat Gomez on 2023-08-14.
+//
+
+import SwiftUI
+
+@main
+struct WeatherEjercicioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
